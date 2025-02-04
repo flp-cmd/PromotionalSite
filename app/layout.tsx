449b9189c753ab6de39627b343b03b940892726f
flp-cmd <felipe.costafm00@gmail.com>
2025-02-04 14:23:26 -0300

    Adding favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,28 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <Provider>{children}</Provider>
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
