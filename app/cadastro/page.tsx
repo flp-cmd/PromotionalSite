68b094edc5eb988fedbca7dc9b588121e9a5e053
flp-cmd <felipe.costafm00@gmail.com>
2025-02-06 13:26:01 -0300

    Adjusts

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -351,7 +351,6 @@ export default function SignupPage() {
   const handleStoryDownload = () => {
     const link = document.createElement("a");
     link.href = "/story.png";
-    link.download = "storyUmBaitaFestival.png";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
